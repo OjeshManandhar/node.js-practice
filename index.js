4fce145858153376668ed774cf33089800cb9f48
OjeshManandhar <ozes.manandhar@gmail.com>
2020-04-12 19:29:58 +0545

    Return html css and js

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
+
+const dirPath = __dirname + '/src/';
 
 const server = http.createServer((req, res) => {
-  console.log('req:', req);
+  console.log('url:', req.url);
+  console.log('dirname:', path.dirname(req.url));
+  console.log('basename:', path.basename(req.url));
+  console.log('extname:', path.extname(req.url));
+
+  console.log('------------------------');
+
+  if (req.url === '/') {
+    console.log('home');
+
+    fs.readFile(dirPath + 'html/home.html', (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+  } else {
+    var basename = null;
+    const extname = path.extname(req.url);
+
+    switch (extname) {
+      case '.html':
+        basename = path.basename(req.url);
+
+        fs.readFile(dirPath + 'html/' + basename, (err, data) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(data);
+          res.end();
+
+          basename = null;
+        });
+        break;
+      case '.css':
+        basename = path.basename(req.url);
+
+        fs.readFile(dirPath + 'css/' + basename, (err, data) => {
+          res.writeHead(200, { 'Content-Type': 'text/css' });
+          res.write(data);
+          res.end();
+
+          basename = null;
+        });
+        break;
+      case '.js':
+        basename = path.basename(req.url);
+
+        fs.readFile(dirPath + 'js/' + basename, (err, data) => {
+          res.writeHead(200, { 'Content-Type': 'text/css' });
+          res.write(data);
+          res.end();
+
+          basename = null;
+        });
+        break;
+    }
+  }
 });
 
-server.listen(3000);
+server.listen(8000);
